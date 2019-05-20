d082ad6e72de68091fecb265d3b5fa8c9e73eeb0
thehivay <thehivay@gmail.com>
2019-05-20 18:01:25 +0300

    Added task sheets

---
+++ hw7_sheet.py
@@ -0,0 +1,25 @@
+import xlsxwriter
+
+workbook = xlsxwriter.Workbook('hw7_sheet.xlsx')
+
+for i in range(7, 22):
+
+    worksheet1 = workbook.add_worksheet('Homework'+str(i))
+
+    sheet1 = (['NAME', 'PRlink', 'NOTE'],
+              ['Дмитрий Горский', '',''], ['Екатерина Гучек', '', ''],
+              ['Роман Завадский', '', ''], ['Денис Копейкин', '',''],
+              ['Дмитрий Лис', '', ''], ['Дмитрий Прусевич', '',''],
+              ['Дарья Силантьева', '', ''], ['Вячеслав Станкевич', '', '']
+    )
+
+    row, col = 0, 0
+
+    for names, prlink, note in sheet1:
+
+        worksheet1.write(row, col,     names)
+        worksheet1.write(row, col + 1, prlink)
+        worksheet1.write(row, col + 2, note)
+        row += 1
+
+workbook.close()
